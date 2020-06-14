659ba04ea65f75b506db113d5ae0aa29ae4ce733
yair.vogel <yair.vogel@mail.huji.ac.il>
2020-06-14 12:17:44 +0300

    about fix

--- static/scripts/about.js
+++ static/scripts/about.js
@@ -5,20 +5,10 @@ $(document).ready(() => {
 
    const change = document.getElementById("change");
    const trig = document.getElementById("trig");
-   const heart = document.getElementById("heart");
-   const credits = document.getElementById("credits");
 
    trig.addEventListener('mouseenter', e => {
        i++;
        i %= words.length;
        change.innerHTML = words[i];
    });
-
-    credits.addEventListener('mouseenter', e => {
-        heart.style.opacity = "1"
-    });
-
-    credits.addEventListener('mouseleave', e => {
-        heart.style.opacity = "0"
-    })
 });
